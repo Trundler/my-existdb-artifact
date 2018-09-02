7fbcbd40c708a992c0fda2b8724dcdd4b8b5de82
Dannes Wessels <dannes@exist-db.org>
2018-09-02 13:26:16 +0200

    Added xquery test

---
+++ src/test/java/net/trundler/xquery/XqueryTest.java
@@ -0,0 +1,12 @@
+package net.trundler.xquery;
+
+import org.exist.test.runner.XSuite;
+import org.junit.runner.RunWith;
+
+@RunWith(XSuite.class)
+@XSuite.XSuiteFiles({
+        "src/test/resources/org/exist/jms/xquery"
+})
+
+public class XqueryTest {
+}
